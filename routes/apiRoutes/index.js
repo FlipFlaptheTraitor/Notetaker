4ea9a4a6a3e00ba33b627e3270e0eefaeee6119c
FlipFlaptheTraitor <flipflapthetraitor@protonmail.com>
2021-05-29 20:05:43 -0400

    need to fix json call error

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -5,14 +5,40 @@ const fs = require('fs');
 
 
 router.get('/api/notes', (req, res) => {
-    res.json(notes);
+    fs.readFile(__dirname + "/db/db.json", 'utf8',  (note)=> {
+        res.json(JSON.parse(note))
+})
 });
+
 
 router.post('/api/notes', (req, res) => {
-    let newNote = req.body;
-    notes.push(newNote);
-
+    fs.writeFile(__dirname + "/db/db.json", 'utf8',  (note)=> {
+        let newNote = req.body.id;
+            note.push(newNote);
+            updateDb();
+})
 });
 
 
+router.get("/api/notes/:id", (req,res)=> {
+    fs.readFile(__dirname + "/db/db.json", 'utf8',  (note)=> {
+    res.json(note[req.params.id]);
+})
+});
+
+
+router.delete("/api/notes/:id", (req, res)=> {
+    fs.writeFile(__dirname + "/db/db.json", 'utf8',  (note)=> {
+        note.splice(req.params.id);
+            updateDb();
+})
+});
+
+
+function updateDb() {
+    fs.writeFile(__dirname +"db/db.json",JSON.stringify(note,'\t'),err => {
+        if (err) throw err;
+        return true;
+    });
+}
 module.exports = router;
